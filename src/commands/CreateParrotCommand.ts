fc9802444917b1bc204f37ef6db7201afbef9d60
Exan <exan@landviz.nl>
2020-08-16 18:11:18 +0200

    Im sick of privacy policies

--- src/commands/CreateParrotCommand.ts
+++ src/commands/CreateParrotCommand.ts
@@ -6,13 +6,19 @@ import { Attachment } from 'discord.js';
 export class CreateParrotCommand extends Command implements IRunnableCommand {
     public async run(): Promise<void> {
 
+		if (this.message.content.includes('privacy')) {
+			this.sendMessage('Parrot does not log or store anything longer than required for the application to function. This bot is hosted on a Scaleway VPS. Their privacy policy may apply.');
+			return;
+		}
+
         if (this.message.attachments.size === 0) {
             this.sendMessage('Attach an image to get a parrot.');
             return;
         }
 
         const input = this.getInput();
-        const speed = input.speed ? Number(input.speed) : 4.75;
+		const speed = input.speed ? Number(input.speed) : 4.75;
+		
 
         this.message.attachments.tap(async (attachment) => {
             try {
